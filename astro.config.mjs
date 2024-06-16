509cf82eecfa276f2108f0ecbdde760ca42f58fd
nagiept <nagi_1074@yahoo.co.jp>
2024-06-16 20:40:16 +0900

    change pass config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nagiept.github.io/',
-	base: '',
+	outDir: "./docs",
+    base: '/nagiept.github.io',
 	compressHTML: false,
 	scopedStyleStrategy: 'class',
 	devToolbar: {
